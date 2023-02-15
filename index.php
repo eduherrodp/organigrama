5caeb3f33b3278ea5de538f2e484958fb9e0d188
Eduardo <eduher.rodp@gmail.com>
2023-02-15 11:42:48 -0500

    develop branche

--- index.php
+++ index.php
@@ -14,37 +14,11 @@ require_once 'php/DBConnection.php';
 $db = DBConnection::getInstance();
 // Obtención de la conexión a la base de datos
 $conn = $db->getConnection();
-
-/**
- * Llenado de objetos de la clase Persona
- */
-
-// Implementación de la clase DBQuery.php
-require_once 'php/DBQuery.php';
-// Instancia de la clase DBQuery
+ncia de la clase DBQuery
 $dbQuery = new DBQuery($conn);
 
-/**
- * Llenado de objetos de la clase Persona
- */
+// Cierre de la conexión a la base de datos y liberación de recursos
 
-// Implementación de la clase Persona.php
-require_once 'php/Persona.php';
-for ( $i=1; $i < 2; $i++ ) {
-    // Instancia de la clase Persona
-    $personaObj = $dbQuery->getPersona($i);
-    echo $personaObj->getId(), " | ", $personaObj->getNombre(), " | ", $personaObj->getCargo(), " | ", $personaObj->getCorreo(), " | ", $personaObj->getTelefono(), " | ", $personaObj->getCv(), " | ", $personaObj->getFoto(), "<br><br>";
-
-    /**
-     * Liberación de recursos
-     */
-    $personaObj = null;
-    $dbQuery = null;
-}
-
-/**
- * Cierre de la conexión a la base de datos y liberación de recursos
- */
 $db->closeConnection();
 $conn = null;
 
