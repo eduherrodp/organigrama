8313de72644380c9373a15a518c97f5c11113d7b
Eduardo <eduher.rodp@gmail.com>
2023-02-23 13:30:03 -0600

    Query completed

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once 'php/DBConnection.php';
  */
 
 $db = DBConnection::getInstance();
-$sql = "SELECT * FROM personas";
+$sql = "SELECT personas.id, personas.nombre, cargos.cargo, correos.correo, telefonos.telefono, cv.cv, fotos.foto FROM personas JOIN cargos ON personas.id_cargo = cargos.id JOIN correos ON personas.id_correo = correos.id JOIN telefonos ON personas.id_telefono = telefonos.id JOIN cv ON personas.id_cv = cv.id JOIN fotos ON personas.id_foto = fotos.id;";
 $stmt = $db->query($sql);
 $rows = $stmt->fetchAll();
 
