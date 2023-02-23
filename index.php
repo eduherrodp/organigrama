26b4622d6be9057977a8860eabe7387dcef3e6ed
Eduardo <eduher.rodp@gmail.com>
2023-02-23 11:56:08 -0600

    develop

--- index.php
+++ index.php
@@ -1,19 +1,35 @@
 <?php
-//Implementación de la clase DBConnection.php
-namespace php;
-
-use DBQuery;
-
-require_once 'php/DBConnection.php';
-
-/**
- * Conexión a la base de datos
- */
-
-// Instancia de la clase DBConnection
-$db = DBConnection::getInstance();
-// Obtención de la conexión a la base de datos
-$conn = $db->getConnection();
-
-// Cerrar conexión
-$db->closeConnection();
+////Implementación de la clase DBConnection.php
+//namespace php;
+//
+//use DBQuery;
+//
+//require_once 'php/DBConnection.php';
+//
+///**
+// * Conexión a la base de datos
+// */
+//
+//// Instancia de la clase DBConnection
+//$db = DBConnection::getInstance();
+//// Obtención de la conexión a la base de datos
+//$conn = $db->getConnection();
+//
+///**
+// * Consulta a la base de datos y llenado de objetos de la clase Persona
+// */
+//
+//// Instancia de la clase DBQuery
+//
+//$query = new DBQuery($conn);
+//
+//// Obtenemos los datos de la persona con ID = 1
+//$persona = $query->getPersona(1);
+//
+//// Imprimimos los datos de la persona
+//echo $persona->getId() . "<br>";
+//echo $persona->getNombre() . "<br>";
+//
+//// Cerrar conexión
+//$db->closeConnection();
+echo 'Versión actual de PHP: ' . phpversion();
--- php/DBConnection.php
+++ php/DBConnection.php
@@ -2,11 +2,12 @@
 
 namespace php;
 
+use MongoDB\Driver\Exception\ConnectionException;
 use PDO;
 use PDOException;
 
 class DBConnection {
-    private static $instance;
+    private static ?DBConnection $instance = null;
     private $conn;
     private $host;
     private $user;
@@ -14,8 +15,9 @@ class DBConnection {
     private $db;
     private $port;
 
-    // Constructor de la clase DBConnection
+    const DRIVER = 'mysql';
 
+    // Constructor de la clase DBConnection
     private function __construct() {
         // Importación de variables de configuración config.php
         require_once 'config.php';
@@ -27,13 +29,21 @@ class DBConnection {
 
         // Creación de la conexión a la base de datos, manejo de errores y excepciones
         try {
-            $this->conn = new PDO("mysql:host=$this->host;port=$this->port;dbname=$this->db", $this->user, $this->pass);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $dsn = self::DRIVER . ":host=$this->host;port=$this->port;dbname=$this->db;charset=utf8mb4";
+            $options = [
+                PDO::ATTR_EMULATE_PREPARES => false, // Desactiva la emulación de consultas para evitar inyección de código
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4' COLLATE 'utf8mb4_unicode_ci" // Establece el juego de caracteres para la conexión
+            ];
+            $this->conn = new PDO($dsn, $this->user, $this->pass, $options);
         } catch (PDOException $e) {
-            echo "Error de conexión: " . $e->getMessage();
+            error_log("Error de conexión a la base de datos", $e->getMessage());
+            http_response_code(500);
+            die("Error de conexión a la base de datos. Por favor, inténtelo de nuevo más tarde");
         }
     }
-    // Implementación del patron Singleton para la conexión a la base de datos, el cual solo permite una instancia de la clase
+    // Singleton
     public static function getInstance() {
         if (!isset(self::$instance)) {
             self::$instance = new DBConnection();
@@ -45,7 +55,33 @@ class DBConnection {
     }
 
     public function getConnection() {
+        // Verificar si la conexión a la base de datos está activa antes de devolverla
+        if ( !$this->conn || $this->conn->getAttribute(PDO::ATTR_CONNECTION_STATUS)) {
+            throw new ConnectionException("No se pudo establecer la conexión a la base de datos");
+        }
         return $this->conn;
     }
 
+    // Ejecutar una consulta en la base de datos y devolver un objeto PDOStatement
+    public function query($sql, $params = []) {
+        $stmt = $this->getConnection()->prepare($sql);
+        $stmt->execute($params);
+        return $stmt;
+    }
+    // Obtener una fila de la base de datos
+    public function getRow($sql, $params = []) {
+        $stmt = $this->query($sql);
+        return $stmt->fetch();
+    }
+
+    // Obtener varias filas de la base datos
+    public function getRows($sql, $params = []) {
+        $stmt = $this->query($sql);
+        return $stmt->fetchAll();
+    }
+
+    // Impide la clonación, serialización y deserialización de la instancia respectivamente
+    private function __clone(){}
+    private function __sleep(){}
+    private function __wakeup(){}
 }
