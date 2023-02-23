86d6fd6d253ed30cdd779e5bc41478e1d7f23154
Eduardo <eduher.rodp@gmail.com>
2023-02-23 13:17:48 -0600

    New connection

--- index.php
+++ index.php
@@ -9,7 +9,26 @@ require_once 'php/DBConnection.php';
  */
 
 $db = DBConnection::getInstance();
-// Show all errors
+$conn = $db->getConnection();
+
+// Consulta a la base de datos
+$sql = "SELECT * FROM persona";
+
+// Utilizacion del método query de la clase DBConnection
+
+$data = $db->query($sql);
+
+// Iteración de los resultados de la consulta
+foreach ($data as $row) {
+    echo $row['nombre'];
+}
+
+// Cierre de la conexión a la base de datos
+$db->closeConnection();
+
+
+
+
 
 
 
