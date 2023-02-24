44dfcb955bb6f4c02e955b6f7a8a8e453f663e6f
Eduardo <eduher.rodp@gmail.com>
2023-02-24 00:22:58 -0600

    mensaje

--- index.php
+++ index.php
@@ -15,10 +15,10 @@ $db = DBConnection::getInstance();
 $stmt = $db->query($sql);
 $rows = $stmt->fetchAll();*/
 
-$sql = "SELECT personas.id, personas.nombre, cargos.cargo, correos.correo, telefonos.telefono, cv.cv, fotos.foto FROM personas JOIN cargos ON personas.id_cargo = cargos.id JOIN correos ON personas.id_correo = correos.id JOIN telefonos ON personas.id_telefono = telefonos.id JOIN cv ON personas.id_cv = cv.id JOIN fotos ON personas.id_foto = fotos.id WHERE personas.id = ?;" [1];
-$result = $db->getRows($sql);
+$sql = "SELECT personas.id, personas.nombre, cargos.cargo, correos.correo, telefonos.telefono, cv.cv, fotos.foto FROM personas JOIN cargos ON personas.id_cargo = cargos.id JOIN correos ON personas.id_correo = correos.id JOIN telefonos ON personas.id_telefono = telefonos.id JOIN cv ON personas.id_cv = cv.id JOIN fotos ON personas.id_foto = fotos.id WHERE personas.id = ?;";
+$result = $db->getRows($sql, [1]);
 // Show the result
-var_dump($result);
+echo $result[0]['nombre'];
 
 // Close the connection
 $db->closeConnection();
