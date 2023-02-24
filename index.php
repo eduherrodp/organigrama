53f973101d0a9d38a42d6454bfb09bcbd5988a86
Eduardo <eduher.rodp@gmail.com>
2023-02-24 00:25:02 -0600

    Query SELECT  WHERE

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $rows = $stmt->fetchAll();*/
 $sql = "SELECT personas.id, personas.nombre, cargos.cargo, correos.correo, telefonos.telefono, cv.cv, fotos.foto FROM personas JOIN cargos ON personas.id_cargo = cargos.id JOIN correos ON personas.id_correo = correos.id JOIN telefonos ON personas.id_telefono = telefonos.id JOIN cv ON personas.id_cv = cv.id JOIN fotos ON personas.id_foto = fotos.id WHERE personas.id = ?;";
 $result = $db->getRows($sql, [1]);
 // Show the result
-echo $result[0];
+var_dump($result[0]); 
 
 // Close the connection
 $db->closeConnection();
