88cd83f65223222f92f200f2e0ecb28d790d88e5
Eduardo <eduher.rodp@gmail.com>
2023-02-28 11:07:56 -0600

    iteration branche

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ $db = DBConnection::getInstance();
 $sql = "SELECT personas.id, personas.nombre, cargos.cargo, correos.correo, telefonos.telefono, cv.cv, fotos.foto FROM personas JOIN cargos ON personas.id_cargo = cargos.id JOIN correos ON personas.id_correo = correos.id JOIN telefonos ON personas.id_telefono = telefonos.id JOIN cv ON personas.id_cv = cv.id JOIN fotos ON personas.id_foto = fotos.id WHERE personas.id = ?;";
 
 for($i = 1; $i<=5; $i++) {
+    echo $i."\n";
     $result = $db->getRows($sql, [$i]);
     // Implementación de la clase Persona
     $persona = new Persona($result[0]['id'], $result[0]['nombre'], $result[0]['cargo'], $result[0]['correo'], $result[0]['telefono'], $result[0]['cv'], $result[0]['foto']);
