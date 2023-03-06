0a95edf6c14be2dac6a4ce1db5a067dc9df962a4
Eduardo <eduher.rodp@gmail.com>
2023-03-05 18:37:03 -0600

    Iteracion

--- index.php
+++ index.php
@@ -26,10 +26,7 @@ for($i = 1; $i<=38; $i++) {
     // Implementación de la clase Persona
     $persona = new Persona($result[0]['id'], $result[0]['nombre'], $result[0]['cargo'], $result[0]['correo'], $result[0]['telefono'], $result[0]['cv'], $result[0]['foto']);
     // Datos persona
-    //echo $persona->getId() . " " . $persona->getNombre() . "\n\n";
-
-    // Liberación de memoria
-
+    echo $persona->getId() . " " . $persona->getNombre() . "\n\n";
 }
 // Close the connection
 $db->closeConnection();
