f7050dc2cc46276e18865a9fadc9954d3671f6c5
Eduardo <eduher.rodp@gmail.com>
2023-03-05 18:32:27 -0600

    Iteracion

--- index.php
+++ index.php
@@ -21,10 +21,10 @@ $sql = "SELECT personas.id, personas.nombre, cargos.cargo, correos.correo, telef
 for($i = 1; $i<=38; $i++) {
     // Complete the query with the id
     $result = $db->getRows($sql, [$i]);
-    echo $result[0]['id'] . " " . $result[0]['nombre'] . "<br>";
+    echo $result[0];
 
     // Implementación de la clase Persona
-    //$persona = new Persona($result[0]['id'], $result[0]['nombre'], $result[0]['cargo'], $result[0]['correo'], $result[0]['telefono'], $result[0]['cv'], $result[0]['foto']);
+    $persona = new Persona($result[0]['id'], $result[0]['nombre'], $result[0]['cargo'], $result[0]['correo'], $result[0]['telefono'], $result[0]['cv'], $result[0]['foto']);
     // Datos persona
     //echo $persona->getId() . " " . $persona->getNombre() . "\n\n";
 
