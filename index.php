0b6a78ea0be4bf3fc02cf13f31d44d6a85a87616
Eduardo <eduher.rodp@gmail.com>
2023-03-05 18:47:50 -0600

    Iteracion

--- index.php
+++ index.php
@@ -24,13 +24,15 @@ for($i = 1; $i<=38; $i++) {
 
     if (!empty($result)) {
         // Implementación de la clase Persona
-        $persona = new Persona($result[0]['id'], $result[0]['nombre'], $result[0]['cargo'], $result[0]['correo'], $result[0]['telefono'], $result[0]['cv'], $result[0]['foto']);
+        /*$persona = new Persona($result[0]['id'], $result[0]['nombre'], $result[0]['cargo'], $result[0]['correo'], $result[0]['telefono'], $result[0]['cv'], $result[0]['foto']);
 
         // Datos persona
         echo $persona->getId() . " " . $persona->getNombre() . "\n\n";
 
         // Liberación de memoria
-        unset($persona);
+        unset($persona);*/
+        // Show result and the type of data
+        var_dump($result);
     } else {
         echo "No se encontró el registro con id " . $i . "\n";
     }
