97828f74d68eaa1a5e396ff0f6da73ada199add0
Eduardo <eduher.rodp@gmail.com>
2023-03-05 18:46:47 -0600

    Iteracion

--- index.php
+++ index.php
@@ -22,15 +22,20 @@ for($i = 1; $i<=38; $i++) {
     // Complete the query with the id
     $result = $db->getRows($sql, [$i]);
 
-    // Implementación de la clase Persona
-    $persona = new Persona($result[0]['id'], $result[0]['nombre'], $result[0]['cargo'], $result[0]['correo'], $result[0]['telefono'], $result[0]['cv'], $result[0]['foto']);
-    
-    // Datos persona
-    // echo $persona->getId() . " " . $persona->getNombre() . "\n\n";
+    if (!empty($result)) {
+        // Implementación de la clase Persona
+        $persona = new Persona($result[0]['id'], $result[0]['nombre'], $result[0]['cargo'], $result[0]['correo'], $result[0]['telefono'], $result[0]['cv'], $result[0]['foto']);
 
-    // Liberación de memoria
-    unset($persona);
+        // Datos persona
+        echo $persona->getId() . " " . $persona->getNombre() . "\n\n";
+
+        // Liberación de memoria
+        unset($persona);
+    } else {
+        echo "No se encontró el registro con id " . $i . "\n";
+    }
 }
+
 // Close the connection
 $db->closeConnection();
 
