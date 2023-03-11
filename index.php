9dda783d3a2b091d29fe2fd48a0553ffe57d512a
Eduardo <eduher.rodp@gmail.com>
2023-03-11 15:09:12 -0600

    error log

--- index.php
+++ index.php
@@ -37,10 +37,10 @@ for($i = 1; $i<=38; $i++) {
         // Liberación de memoria
         unset($persona);
         $msj = "[OK query]'  " . date('d-m-Y H:i:s') . ": Found register whit id " . $i;
-        error_log("$msj", 3, ", error_log.txt");
+        error_log("$msj", 3, "error_log.txt");
     } else {
         $msj = "[ERROR query]'  " . date('d-m-Y H:i:s') . ": Not found register whit id " . $i;
-        error_log("$msj", 3, ", error_log.txt");
+        error_log("$msj", 3, "error_log.txt");
     }
 }
 
